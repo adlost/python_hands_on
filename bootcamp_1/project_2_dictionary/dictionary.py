3e004c159d386d534b5ea86361f7d50c5ace4360
AdLost <andlozedd@gmail.com>
2022-10-11 08:09:16 +0300

    new def

--- bootcamp_1/project_2_dictionary/dictionary.py
+++ bootcamp_1/project_2_dictionary/dictionary.py
@@ -1,2 +1,15 @@
+import json
+
+data = json.load(open("data.json"))
+
+
+def translate(word):
+    if word in data:
+        return data[word]
+    else:
+        print("Pugger you paw steps on wrong keys")
+
 
 word = input("Enter the word you want to search: ")
+output = translate(word)
+print(output)
